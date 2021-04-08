b1d21a68e33020872c97e80255f64a8faa739674
kcsarith <krisnasarith@gmail.com>
2021-04-08 11:46:23 -0700

    Create question.js

---
+++ backend/models/question.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = monotype.Schema;
+
+const QuestionSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    repo: {
+        type: String,
+    },
+    live: {
+        type: String,
+    },
+    isAnswered: {
+        type: Boolean,
+        default: false,
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = Question = mongoose.model("Question", QuestionSchema);
